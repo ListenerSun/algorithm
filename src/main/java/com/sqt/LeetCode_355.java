12d35c20ae00aa29fd35d6e39cdc85a82d6554d2
ListenerSun <ListenerSun@163.com>
2020-04-21 00:10:33 +0800

    寻找两个数组的交集

---
+++ src/main/java/com/sqt/LeetCode_355.java
@@ -0,0 +1,65 @@
+package com.sqt;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2,2]
+ * 示例 2:
+ * <p>
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [4,9]
+ * 说明：
+ * <p>
+ * 输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+ * 我们可以不考虑输出结果的顺序。
+ * 进阶:
+ * <p>
+ * 如果给定的数组已经排好序呢？你将如何优化你的算法？
+ * 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+ * 如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Description:
+ * @author: ListenerSun(男, 未婚) 微信:810548252
+ * @Date: Created in 2020-04-20 23:51
+ */
+public class LeetCode_355 {
+
+    public static void main(String[] args) {
+        int[] nums1 = {4, 9, 5}, nums2 = {9, 4, 9, 8, 4};
+        Arrays.stream(intersect(nums1, nums2)).forEach(System.out::println);
+
+    }
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int index = 0;
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                nums1[index++] = num;
+                map.put(num, map.get(num) - 1);
+                if (map.get(num) == 0) {
+                    map.remove(num);
+                }
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, index);
+    }
+}
