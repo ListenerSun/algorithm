5d1062fc667f8a31749e2775fc8de5192322706f
ListenerSun <ListenerSun@163.com>
2020-05-09 11:51:16 +0800

    add 合并两个有序数组

--- src/main/java/com/sqt/LeetCode_20.java
+++ src/main/java/com/sqt/LeetCode_20.java
@@ -7,37 +7,37 @@ import java.util.Stack;
  * @Description:
  * @author: ListenerSun(男, 未婚) 微信:810548252
  * @Date: Created in 2020-05-07 19:28
- *
- *
+ * <p>
+ * <p>
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
- *
+ * <p>
  * 有效字符串需满足：
- *
+ * <p>
  * 左括号必须用相同类型的右括号闭合。
  * 左括号必须以正确的顺序闭合。
  * 注意空字符串可被认为是有效字符串。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: "()"
  * 输出: true
  * 示例 2:
- *
+ * <p>
  * 输入: "()[]{}"
  * 输出: true
  * 示例 3:
- *
+ * <p>
  * 输入: "(]"
  * 输出: false
  * 示例 4:
- *
+ * <p>
  * 输入: "([)]"
  * 输出: false
  * 示例 5:
- *
+ * <p>
  * 输入: "{[]}"
  * 输出: true
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/valid-parentheses
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -46,7 +46,7 @@ public class LeetCode_20 {
 
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for (char c: s.toCharArray()) {
+        for (char c : s.toCharArray()) {
             if (c == '{') {
                 stack.push('}');
             } else if (c == '[') {
